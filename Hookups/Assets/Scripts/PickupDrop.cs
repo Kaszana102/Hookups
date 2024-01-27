bd9a7ba29fc495996c33979e24486b2668805dc1
Jakub <kubawierzba1@wp.pl>
2024-01-27 15:56:28 +0100

    Multiplayer

--- Hookups/Assets/Scripts/PickupDrop.cs
+++ Hookups/Assets/Scripts/PickupDrop.cs
@@ -16,31 +16,35 @@ public class PickupDrop : MonoBehaviour
 
     public void OnGrab(InputAction.CallbackContext context){
         if (grabbable == null)
+        {
+            float pickupDistance = 30f;
+            if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out RaycastHit raycastHit, pickupDistance))
             {
-                float pickupDistance = 30f;
-                if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out RaycastHit raycastHit, pickupDistance))
+                Debug.Log(raycastHit);
+                if (raycastHit.transform.TryGetComponent(out grabbable))
                 {
-                    Debug.Log(raycastHit);
-                    if (raycastHit.transform.TryGetComponent(out grabbable))
-                    {
-                        grabbable.grab(objectGrabPointTransform);
-                        animator.SetBool("Holding", true);
-                }
+                    grabbable.grab(objectGrabPointTransform);
+                    animator.SetBool("Holding", true);
                 }
             }
-            else
-            {
-                grabbable.drop();
-            animator.SetBool("Holding", false);
-            grabbable = null;
-            }
+        }        
     }
 
     public void OnThrow(InputAction.CallbackContext context)
     {
         if (grabbable != null){
             grabbable.throwObject();
+            animator.SetTrigger("Throw");
         }
+    }
 
+    public void OnReleaseHand(InputAction.CallbackContext context)
+    {
+        if(grabbable!=null)
+        {
+            grabbable.drop();
+            animator.SetBool("Holding", false);
+            grabbable = null;
+        }
     }
 }
