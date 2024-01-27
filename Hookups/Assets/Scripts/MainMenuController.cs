95eea96e81dcd3aecd8c7c0304b18c083540f2e4
Workraft <ful@onet.eu>
2024-01-27 02:03:23 +0100

    Menu & Scene Change

---
+++ Hookups/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    public Button exit;
+    public int scene = 2;
+    static bool wasPlaying = false;
+    public GameObject firstMenu, secondMenu;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        exit.onClick.AddListener(AppQuit);
+        if(wasPlaying == true)
+        {
+            firstMenu.SetActive(false);
+            secondMenu.SetActive(true);
+        }
+    }
+    public void AppQuit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setForest()
+    {
+        scene = 2;
+        Debug.Log("Map set to forrest");
+    }
+    
+    public void loadScene()
+    {
+        switch (scene)
+        {
+            case 2:
+                AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("FarmScene");
+                break;
+            default:
+                Debug.Log("Error");
+                break;
+        }
+    }
+    public static void returnToMenu()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Menu");
+        wasPlaying = true;
+    }
+}
