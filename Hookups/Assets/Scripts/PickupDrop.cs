2efc3e4513644df93ba543513b6d5fdbf7b2b163
Kacper Kujawa <kacperkujawa@MacBook-Pro-Kacper.local>
2024-01-27 12:38:33 +0100

    Implement object grabbing and dropping

--- Hookups/Assets/Scripts/PickupDrop.cs
+++ Hookups/Assets/Scripts/PickupDrop.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PickupDrop : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    [SerializeField] private Transform playerCameraTransform;
+    [SerializeField] private Transform objectGrabPointTransform;
+    [SerializeField] private LayerMask pickupLayerMask;
+    private IGrabbable grabbable;
+
+
+    public void OnGrab(InputAction.CallbackContext context){
+        if (grabbable == null)
+            {
+                float pickupDistance = 2f;
+                if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out RaycastHit raycastHit, pickupDistance))
+                {
+                    Debug.Log(raycastHit);
+                    if (raycastHit.transform.TryGetComponent(out grabbable))
+                    {
+                        grabbable.grab(objectGrabPointTransform);
+                    }
+                }
+            }
+            else
+            {
+                grabbable.drop();
+                grabbable = null;
+            }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
